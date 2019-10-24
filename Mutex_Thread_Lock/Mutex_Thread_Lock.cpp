b6cf9f696669019264f30da6ad8558b8b2656930
JASON L JOHNSON <JASJOHN3@uat.edu>
2019-10-24 11:54:45 -0700

    Add project files.

---
+++ Mutex_Thread_Lock/Mutex_Thread_Lock.cpp
@@ -0,0 +1,43 @@
+ // Mutex_Thread_Lock.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// mutex::lock/unlock
+#include <iostream>       // std::cout
+#include <thread>         // std::thread
+#include <mutex>          // std::mutex
+
+std::mutex mtx;           // mutex for critical section
+
+void print_thread_id(int id) {
+	// critical section (exclusive access to std::cout signaled by locking mtx):
+	mtx.lock();
+	std::cout << "thread #" << id << '\n';
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout << "Thread Loop Test: " << i << " ." << std::endl;
+		system("pause");
+	}
+	mtx.unlock();
+}
+
+int main()
+{
+	std::thread threads[10];
+	// spawn 10 threads:
+	for (int i = 0; i < 10; ++i)
+		threads[i] = std::thread(print_thread_id, i + 1);
+
+	for (auto& th : threads) th.join();
+	system("pause");
+	return 0;
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
